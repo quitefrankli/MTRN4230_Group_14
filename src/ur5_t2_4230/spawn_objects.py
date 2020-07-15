7df097212470688ecbf988be752e238b8facc9ea
Kanadech King <kanadech.king@gmail.com>
2020-07-15 08:37:18 -0700

    Randomly generated 10 objects

--- src/ur5_t2_4230/spawn_objects.py
+++ src/ur5_t2_4230/spawn_objects.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-# Author: Frank Li - z5115761
 # Creation Date: 04/07/2020
 # Last Modified: 05/07/2020
 # Description:
@@ -9,12 +8,15 @@
 import tf
 import rospy
 # import typing
+import random
 from std_msgs.msg import String
 from gazebo_msgs.srv import SpawnModel, DeleteModel
 from geometry_msgs.msg import Pose, Point, Quaternion
 
+
 def test():
     print("hi")
+
 
 class URDF_Object:
     orientation = Quaternion(0, 0, 0, 0)
@@ -25,15 +27,14 @@ class URDF_Object:
     def __init__(self, node_name, object_name, point):
         '''Args: str, Point'''
 
-        # first try to delete it (it might already exist)
         self.delete_model(node_name)
-
         MODEL_PATH = rospy.get_param('MODEL_PATH') + 'urdf/'
 
         with open(MODEL_PATH + object_name + '.urdf') as file:
             xml = file.read()
             item_pose = Pose(point, self.orientation)
             self.spawn_model(node_name, xml, '', item_pose, 'world')
+
 
 class SDF_Object:
     orientation = Quaternion(0, 0, 0, 0)
@@ -44,9 +45,7 @@ class SDF_Object:
     def __init__(self, node_name, object_name, pose):
         '''Args: str, Point'''
 
-        # first try to delete it (it might already exist)
         self.delete_model(node_name)
-        
         MODEL_PATH = rospy.get_param('MODEL_PATH') + 'sdf/'
 
         with open(MODEL_PATH + object_name + '.sdf') as file:
@@ -54,30 +53,54 @@ class SDF_Object:
             # item_pose = Pose(point, self.orientation)
             self.spawn_model(node_name, xml, '', pose, 'world')
 
+
+def get_randomized_xy():
+    offset = 0.05
+    y = random.uniform(-0.27305 + offset, 0.27305 - offset)
+    x = random.uniform(-0.0675 + offset - 0.019050, 0.25 - offset - 0.019050)
+    return x, y
+
+
 def main():
     print("Waiting for gazebo services...")
     rospy.init_node("spawn_products_in_bins")
     rospy.wait_for_service("gazebo/spawn_urdf_model")
     rospy.wait_for_service("gazebo/spawn_sdf_model")
     rospy.wait_for_service("gazebo/delete_model")
-    print("Got it.")
 
-    pose = Pose(Point(0.25, 0, 0.1), Quaternion(0, 1, 0, -1))
-    input_container = SDF_Object('input_container', 'container', pose)
-    pose = Pose(Point(0, 2, 0.1), Quaternion(0, 1, 0, -1))
-    output_container = SDF_Object('output_container', 'container', pose)
+    # Quaternion is not being used
+    input_container_x = 0.25
+    input_container_y = 0
+    input_container = SDF_Object('input_container', 'container', Pose(
+        Point(input_container_x, input_container_y, 0.1), Quaternion(0, 1, 0, -1)))
+    output_container = SDF_Object('output_container', 'container', Pose(
+        Point(0, 2, 0.1), Quaternion(0, 1, 0, -1)))
 
-    red_box = URDF_Object('red_box', 'red_box', Point(0, 0, 0.2))
-    green_box = URDF_Object('green_box', 'green_box', Point(0, -0.2, 0.2))
-    blue_box = URDF_Object('blue_box', 'blue_box', Point(0, 0.2, 0.2))
-    rate = rospy.Rate(10) # 10hz
+    object_list = ['red_box', 'green_box', 'blue_box',
+                   'red_cylinder', 'green_cylinder', 'blue_cylinder']
 
-    while not rospy.is_shutdown():
-        URDF_Object('green_box %s' % rospy.get_time(), 'green_box', Point(0, -0.2, 0.2))
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        rate.sleep()
+    used_coordinates = list()
 
+    for i in range(10):
+        if len(used_coordinates) == 0:
+            x, y = get_randomized_xy()
+            choice = random.choice(object_list)
+            URDF_Object("object_%d" % (i), choice, Point(x, y, 0.2))
+            used_coordinates.append((x, y))
+            continue
+        found = False
+        while not found:
+            x, y = get_randomized_xy()
+            for coor in used_coordinates:
+                if ((x-0.025*2) <= coor[0] <= (x+0.025*2)) and ((y-0.025*2) <= coor[1] <= (y+0.025*2)):
+                    print("Collide")
+                    found = False
+                    break
+                else:
+                    found = True
+        choice = random.choice(object_list)
+        URDF_Object("object_%d" % (i), choice, Point(x, y, 0.2))
+        used_coordinates.append((x, y))
 
 if __name__ == '__main__':
     try:

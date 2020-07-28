cc0864d0f5f68361ee619294d3b7e9faf8d905f5
gloriayt <gloriaytai@gmail.com>
2020-07-28 02:31:30 -0700

    Preliminary working depth image cv2 image

--- src/ur5_t2_4230/read_image.py
+++ src/ur5_t2_4230/read_image.py
@@ -10,10 +10,10 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class image_converter:
+class colour_image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image)
+    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=10)
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
@@ -28,16 +28,45 @@ class image_converter:
     if cols > 60 and rows > 60 :
       cv2.circle(cv_image, (50,50), 10, 255)
 
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
+    cv2.imshow("Colour Image", cv_image)
+    cv2.waitKey(0)
 
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
     except CvBridgeError as e:
       print(e)
 
+class depth_image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("image_topic_3",Image,queue_size=10)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.callback) 
+
+  def callback(self,data):
+    try:
+      data.encoding = "32FC1"
+      cv_image = self.bridge.imgmsg_to_cv2(data, "32FC1")
+      cv2.normalize(cv_image,cv_image,0,1,cv2.NORM_MINMAX)
+    except CvBridgeError as e:
+      print(e)
+
+    (rows,cols) = cv_image.shape
+    if cols > 60 and rows > 60 :
+      cv2.circle(cv_image, (50,50), 10, 255)
+
+    cv2.imshow("Depth Image", cv_image)
+    cv2.waitKey(0)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "32FC1"))
+    except CvBridgeError as e:
+      print(e)
+
 def main(args):
-  ic = image_converter()
+  #colour_image_converter()
+  depth_image_converter()
   rospy.init_node('image_converter', anonymous=True)
   try:
     rospy.spin()

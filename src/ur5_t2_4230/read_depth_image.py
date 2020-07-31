411c35b9c62939eed1ba435f8c35c363fb406efb
Kanadech King <kanadech.king@gmail.com>
2020-07-31 16:35:53 +1000

    update image scripts

--- src/ur5_t2_4230/read_depth_image.py
+++ src/ur5_t2_4230/read_depth_image.py
@@ -10,36 +10,11 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class colour_image_converter:
-
-  def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=10)
-
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/color/image_raw",Image,self.callback)
-
-  def callback(self,data):
-    try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
-
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
-
-    cv2.imshow("Colour Image", cv_image)
-    cv2.waitKey(0)
-
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 class depth_image_converter:
 
   def __init__(self):
-    self.image_pub = rospy.Publisher("image_topic_3",Image,queue_size=10)
+    self.image_pub = rospy.Publisher("cv_depth_image",Image,queue_size=10)
 
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/depth/image_raw",Image,self.callback) 
@@ -52,10 +27,6 @@ class depth_image_converter:
     except CvBridgeError as e:
       print(e)
 
-    (rows,cols) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
-
     cv2.imshow("Depth Image", cv_image)
     cv2.waitKey(0)
 
@@ -65,9 +36,8 @@ class depth_image_converter:
       print(e)
 
 def main(args):
-  #colour_image_converter()
   depth_image_converter()
-  rospy.init_node('image_converter', anonymous=True)
+  rospy.init_node('image_converter_depth', anonymous=True)
   try:
     rospy.spin()
   except KeyboardInterrupt:

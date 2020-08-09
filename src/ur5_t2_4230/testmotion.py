c2c129a92e0b6ffd23691377db86ab9641dc6d90
gloriayt <gloriaytai@gmail.com>
2020-08-08 21:43:05 -0700

    Added basic object avoidance

--- src/ur5_t2_4230/testmotion.py
+++ src/ur5_t2_4230/testmotion.py
@@ -5,14 +5,33 @@
 
 from std_msgs.msg import Header
 from trajectory_msgs.msg import JointTrajectory
-
-from trajectory_msgs.msg import JointTrajectoryPoint
+from tf.transformations import quaternion_from_euler
 import rospy
+import sys
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
 
-waypoints = [[0.0, -1.44, 1.4, 0.6, 0, -0.33], [0,0,0,0,0,0]]
+#waypoints = [[0.0, -1.44, 1.4, 0.6, 0, -0.33], [0,0,0,0,0,0]]
 
 def main():
+    robot = moveit_commander.RobotCommander()
 
+    group = moveit_commander.MoveGroupCommander("manipulator")
+    pose_target = geometry_msgs.msg.Pose()
+    pose_target.orientation.w = 1.0
+    pose_target.position.x = 0.2
+    pose_target.position.y = 0.6
+    pose_target.position.z = 0.25
+
+    # Plans path to avoid collision objects in Planning Scene:
+    group.set_planner_id("RRTConnectkConfigDefault")
+    group.set_pose_target(pose_target)
+    group.plan()
+    group.go(wait=True)
+    rospy.spin()
+
+    """
     rospy.init_node('send_joints')
     pub = rospy.Publisher('/arm_controller/command',
                           JointTrajectory,
@@ -47,7 +66,7 @@ def main():
         # Publish the message
         pub.publish(traj)
         rate.sleep()
-
+        """
 if __name__ == '__main__':
     try:
         main()
